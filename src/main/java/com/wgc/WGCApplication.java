40cc3b53348da25e9405bb1747494efccc855d1a
daisy <daisy01@missfresh.cn>
2018-06-02 14:33:47 +0800

    代码生成器 init

---
+++ src/main/java/com/wgc/WGCApplication.java
@@ -0,0 +1,25 @@
+package com.wgc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@ServletComponentScan
+@MapperScan("com.wgc.*.dao")
+@SpringBootApplication
+public class WGCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WGCApplication.class, args);
+        System.out.println("ヾ(◍°∇°◍)ﾉﾞ    wgc启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+                " ______                    _   ______            \n" +
+                "|_   _ \\                  / |_|_   _ `.          \n" +
+                "  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+                "  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+                " _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+                "|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
+    }
+}
